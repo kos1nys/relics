19b63e3633b5c5b0712925693f7175c843ef79f1
kosinys <129779184+kos1nys@users.noreply.github.com>
2023-11-27 22:13:11 +0300

    Update RingOfDarkness.java

--- src/main/java/it/hurts/sskirillss/relics/items/relics/ring/RingOfDarkness.java
+++ src/main/java/it/hurts/sskirillss/relics/items/relics/ring/RingOfDarkness.java
@@ -1,1 +1,59 @@
+package it.hurts.sskirillss.relics.items.relics.ring;
 
+import it.hurts.sskirillss.relics.client.particles.circle.CircleTintData;
+import it.hurts.sskirillss.relics.client.tooltip.base.RelicStyleData;
+import it.hurts.sskirillss.relics.init.ItemRegistry;
+import it.hurts.sskirillss.relics.items.relics.base.RelicItem;
+import it.hurts.sskirillss.relics.items.relics.base.data.base.RelicData;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityData;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityEntry;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicAbilityStat;
+import it.hurts.sskirillss.relics.items.relics.base.data.leveling.RelicLevelingData;
+import it.hurts.sskirillss.relics.items.relics.base.utils.AbilityUtils;
+import it.hurts.sskirillss.relics.items.relics.base.utils.LevelingUtils;
+import it.hurts.sskirillss.relics.utils.EntityUtils;
+import it.hurts.sskirillss.relics.utils.MathUtils;
+import it.hurts.sskirillss.relics.utils.ParticleUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.entity.EntityTeleportEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.SlotContext;
+
+import javax.annotation.Nullable;
+import java.awt.*;
+
+public class RingOfDarkness extends RelicItem {
+    @Override
+    public RelicData constructRelicData() {
+        return RelicData.builder()
+                .abilityData(RelicAbilityData.builder()
+                        .ability("speedofdark", RelicAbilityEntry.builder()
+                                .maxLevel(0)
+                                .build())
+                        .ability("dark", RelicAbilityEntry.builder()
+                                .requiredLevel(5)
+                                .requiredPoints(2)
+                                .stat("rolls", RelicAbilityStat.builder()
+                                        .initialValue(0D, 1D)
+                                        .upgradeModifier(RelicAbilityStat.Operation.ADD, 1D)
+                                        .formatValue(value -> (int) MathUtils.round(value, 0))
+                                        .build())
+                                .build())
+                        .build())
+                .levelingData(new RelicLevelingData(100, 10, 200))
+                .styleData(RelicStyleData.builder()
+                        .borders("#008cd7", "#0a3484")
+                        .build())
+                .build();
+    }
